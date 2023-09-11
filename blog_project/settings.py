b1d029aa848ab158bca4ac1b490a701d2a8896de
minsuhaha <haminsu5@gmail.com>
2023-09-11 13:40:21 +0900

    Init

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db', # 데이터베이스 이름
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
