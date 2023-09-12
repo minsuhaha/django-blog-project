fed269cb91608ff4b01ac09a80d8e6c0ac394793
minsuhaha <haminsu5@gmail.com>
2023-09-12 17:52:08 +0900

    Update 최문정 / 게시물리스트 board url  추가

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   
+   path('', views.board, name='board'),
    path('write/', views.board_write, name='board_write'),
    path('login/', views.board_login, name='board_login'), 
 ]
