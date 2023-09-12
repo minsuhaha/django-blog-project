3de9dc683a953f4060ea0be7017c606f72194496
moon <moonj96@gmail.com>
2023-09-12 17:53:16 +0900

    Add <최문정 / board.html , board.css, views.board>

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def board(request):
+    posts = Post.objects.all().order_by('-create_date')
+    return render(request, 'board.html', {'posts': posts})
