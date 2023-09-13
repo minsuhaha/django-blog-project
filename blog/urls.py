c3c42fab24d939973a22823c317e2b22430658a9
hoi <coghd28@naver.com>
2023-09-13 14:06:40 +0900

    Add 김채홍 / urls.py에 디테일 연결

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
    path('', views.board, name='board'),
    path('write/', views.board_write, name='board_write'),
    path('login/', views.board_login, name='board_login'), 
+   path('detail/', views.board_detail, name='board_detail')
 ]
