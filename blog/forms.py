5aeec173e3b7746d1b90b6501ccb3283ea95fe71
minsuhaha <haminsu5@gmail.com>
2023-09-14 16:32:21 +0900

    Update 하민수/forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,15 +1,16 @@
 from django import forms
 from .models import Post, Topic
+from ckeditor.widgets import CKEditorWidget  # CKEditor 위젯 추가
 
 
 # 게시물 폼
 class BlogForm(forms.ModelForm):
 
-    topic = forms.ModelChoiceField(
-        queryset=Topic.objects.all(),
-        label='토픽 선택',
-        widget=forms.RadioSelect  # 라디오 선택 위젯 사용
-    )
+    # topic = forms.ModelChoiceField(
+    #     queryset=Topic.objects.all(),
+    #     label='토픽 선택',
+    #     widget=forms.RadioSelect  # 라디오 선택 위젯 사용
+    # )
 
     class Meta:
         model = Post
