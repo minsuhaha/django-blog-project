ea2bedaa8dead3ff914618ef99cf0b3fe0d6a726
minsuhaha <haminsu5@gmail.com>
2023-09-14 14:23:05 +0900

    Update 하민수/ blog.urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,16 @@
+# urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.board, name='board'),
-   path('write/', views.board_write, name='board_write'),
    path('login/', views.board_login, name='board_login'), 
    path('logout/', views.board_logout, name='board_logout'), 
-   path('board_detail/', views.board_detail, name= 'board_detail'),
+   
+   path('', views.board, name='board'),
+   path('<int:topic>/', views.board, name='board_topic'),
+
+   path('write/', views.create_or_update_post, name='board_write'),
+   path('edit_post/<int:post_id>/', views.create_or_update_post, name='board_write'),
+   
+   path('board_detail/<int:post_id>/', views.board_detail, name= 'board_detail'),
 ]
