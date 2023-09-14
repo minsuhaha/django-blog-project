9369af0da79da9e8b995a93c0cf0ea2db235b98b
minsuhaha <haminsu5@gmail.com>
2023-09-14 10:09:09 +0900

    Update 하민수 / views.py

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,60 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, logout,authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
 from .forms import BlogForm
 from .models import Post
 
-# 게시글 작성
+
+# 게시물 작성 - board write page
 def board_write(request):
-    if request.method == 'POST':
-        form = BlogForm(request.POST, request.FILES)  # request.FILES를 사용하여 이미지 처리
-        print(form)
-        if form.is_valid():
-            # 폼 데이터를 모델에 저장
-            form = form.save(commit=False)
-            form.writer = request.user  # 현재 로그인한 사용자를 작성자로 설정
-            form.save()
-            return redirect('board')  # 성공 시 홈 페이지로 리디렉션
-    else:
-        form = BlogForm()
+    if request.user.is_authenticated:
+        # 사용자가 로그인한 경우에만 실행
+        if request.method == 'POST':
+            form = BlogForm(request.POST, request.FILES)  # request.FILES를 사용하여 이미지 처리
+            print(form)
+            if form.is_valid():
+                # 폼 데이터를 모델에 저장
+                form = form.save(commit=False)
+                form.writer = request.user  # 현재 로그인한 사용자를 작성자로 설정
+                form.save()
+                return redirect('board')  # 성공 시 홈 페이지로 리디렉션
+        else:
+            form = BlogForm()
     return render(request, 'board_write.html', {'form': form})
 
 
-# 사용자 로그인 뷰
+# 게시물 수정 - board write page
+@login_required(login_url='board_login')
+def board_modify(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.user != post.writer:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('board_detail', post_id=post.id)
+    if request.method == "POST":
+        form = BlogForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('board_detail', post_id=post.id)
+    else:
+        form = BlogForm(instance=post)
+    context = {'form': form}
+    return render(request, 'board_write.html', context)
+
+# 게시물 삭제 - board detail page
+@login_required(login_url='board_login')
+def board_delete(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.user != post.writer:
+        messages.error(request, '삭제권한이 없습니다')
+        return redirect('board_detail', post_id=post.id)
+    post.delete()
+    return redirect('board')
+
+
+# 로그인 - login page
 def board_login(request):
     if request.method == 'POST':
         form = AuthenticationForm(request=request, data=request.POST)
@@ -31,18 +65,28 @@ def board_login(request):
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
 
-# 로그아웃 
+
+# 로그아웃 - logout page
 def board_logout(request):
     logout(request)
     return redirect('board')
 
-# 메인 페이지
-def board(request):
-    posts = Post.objects.all().order_by('-create_date')
-    title_post = Post.objects.order_by('-view').first()
+
+
+# 메인 게시판 - board page
+def board(request, topic=None):
+
+    if topic:
+        posts = Post.objects.filter(topic=topic, publish='Y').order_by('-views')
+    else:
+        posts = Post.objects.filter(publish='Y').order_by('-views') 
+    
+    title_post = Post.objects.all().order_by('-view').first()
     return render(request, 'board.html', {'posts': posts ,'title_post':title_post})
 
-# 상세 페이지
+
+
+# 상세 페이지 - board detail page
 def board_detail(request,id):
     post_detail = Post.objects.filter('id' == id)
     return render(request, 'board_detail.html')
