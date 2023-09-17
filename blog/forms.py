a34650c7742fc1e25a824d0900ecb03c4a039d4b
minsuhaha <haminsu5@gmail.com>
2023-09-17 22:29:18 +0900

    Addd 하민수 / AnswerForm

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post, Topic
+from .models import Post, Topic, Answer
 from ckeditor.widgets import CKEditorWidget  # CKEditor 위젯 추가
 
 
@@ -16,5 +16,13 @@ class BlogForm(forms.ModelForm):
         model = Post
         fields = ['title', 'content', 'topic']
 
+# 답변 폼 추가생성 (9/17 폼 생성완료)
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
+        }
 
 
