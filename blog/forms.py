01552b871a4225f31f3be9d4e7deca33d75a83c6
minsuhaha <haminsu5@gmail.com>
2023-09-18 10:09:34 +0900

    Delete <하민수 : AnswerForm>

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post, Topic, Answer
+from .models import Post, Topic
 from ckeditor.widgets import CKEditorWidget  # CKEditor 위젯 추가
 
 
@@ -17,12 +17,12 @@ class BlogForm(forms.ModelForm):
         fields = ['title', 'content', 'topic']
 
 # 답변 폼 추가생성 (9/17 폼 생성완료)
-class AnswerForm(forms.ModelForm):
-    class Meta:
-        model = Answer
-        fields = ['content']
-        labels = {
-            'content': '답변내용',
-        }
+# class AnswerForm(forms.ModelForm):
+#     class Meta:
+#         model = Answer
+#         fields = ['content']
+#         labels = {
+#             'content': '답변내용',
+#         }
 
 
