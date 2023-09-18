3e836e93016b845e45a3496978b41c6fe54a2c0b
maro <w1193@naver.com>
2023-09-18 14:18:01 +0900

    Update <허승범 / 댓글 작성기능>

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Topic, Post, User
+from .models import Topic, Post, User, Content
 
 class TopicSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,4 +14,9 @@ class PostSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+class ContentSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Content
         fields = '__all__'
